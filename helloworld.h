231c1e839bd07742dafa7e7bdb5ac26d3ab15f5c
TheNachoBIT <43283743+TheNachoBIT@users.noreply.github.com>
2022-10-20 12:08:50 -0300

    First commit :D

---
+++ helloworld.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+struct HelloWorld
+{
+	void English()
+	{
+		std::cout << "Hello World!" << std::endl;
+	}
+
+	void Spanish()
+	{
+		std::cout << "Hola mundo!" << std::endl;
+	}
+};
